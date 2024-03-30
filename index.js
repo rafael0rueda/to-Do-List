04ccd713236b42df8b07a38d44af424269215288
rafaelRueda <rrrm85@gmail.com>
2024-03-30 11:06:26 -0500

    Redesing the basic desing of the project

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-// let checkboxes = document.getElementById("checkboxes");
-// let buttonAdd = document.getElementById("button-add");
-// let listTask = document.getElementById("list-task");
-// let add = document.getElementById("add");
-
 // Click add button -> open where to write new task -> add new task to list
 
-let loginForm = document.getElementById("loginForm");
+let newTask = document.getElementById("new-task");
+let buttonAdd = document.getElementById("button-list");
+let listTask = document.getElementById("list-task");
 
-loginForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  let username = document.getElementById("username");
-  let password = document.getElementById("password");
-
-  if (username.value == "" || password.value == "") {
-    alert("Ensure you input a value in both fields!");
-  } else {
-    // perform operation with form input
-    console.log(username.innerText);
-    alert("This form has been successfully submitted!");
-    console.log(
-      `This form has a username of ${username.value} and password of ${password.value}`
-    );
-
-    username.value = "";
-    password.value = "";
-  }
+buttonAdd.addEventListener("click", () => {
+  let task = document.createElement("li");
+  task.classList.add("circle");
+  task.appendChild(document.createTextNode(newTask.value));
+  listTask.appendChild(task);
+  newTask.value = "";
 });
-
-// var button = document.getElementById("my-button");
-// button.addEventListener("click", changeColor);
-
-// function changeColor() {
-//   document.getElementById("my-div").style.backgroundColor = "green";
-// }
