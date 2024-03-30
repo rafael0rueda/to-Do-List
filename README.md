9bd099937c2b3abe6b685ad07e8778390e12cbbb
rafaelRueda <rrrm85@gmail.com>
2024-03-30 16:02:19 -0500

    Feature add and remove items list

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # To do List
 
 This a project to track and follow task, make with CSS, HTML and JavaScript
+
+### Details
+
+Fonts: Leckerli One
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// Click add button -> open where to write new task -> add new task to list
-
 let newTask = document.getElementById("new-task");
 let buttonAdd = document.getElementById("button-list");
 let listTask = document.getElementById("list-task");
 
+// Add item to the list
 buttonAdd.addEventListener("click", () => {
   let task = document.createElement("li");
   task.classList.add("circle");
   task.appendChild(document.createTextNode(newTask.value));
   listTask.appendChild(task);
   newTask.value = "";
+});
+
+// Remove item from the list
+listTask.addEventListener("click", (e) => {
+  listTask.removeChild(e.target);
 });
